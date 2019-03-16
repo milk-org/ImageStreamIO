340a8566a8894c560f3a83a06d703a195708c0cb
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2019-03-16 11:43:18 +0100

    reformat ImageStruct.t to be more readable python_build disable by default

--- ImageStruct.h
+++ ImageStruct.h
@@ -63,86 +63,76 @@ extern "C" {
 #endif
 
 // comment this line if data should not be packed
-// packing data should be use with extreme care, so it is recommended to disable
-// this feature
+// packing data should be use with extreme care, so it is recommended to disable this feature
 //#define DATA_PACKED
 
-#define SHAREDMEMDIR "/tmp" /**< location of file mapped semaphores */
+#define SHAREDMEMDIR    "/tmp"            /**< location of file mapped semaphores */
 
-#define SEMAPHORE_MAXVAL                                                       \
-  10 /**< maximum value for each of the semaphore, mitigates warm-up time when \
-        processes catch up with data that has accumulated */
-#define SEMAPHORE_INITVAL                                                     \
-  0 /**< maximum value for each of the semaphore, mitigates warm-up time when \
-       processes catch up with data that has accumulated */
-#define IMAGE_NB_SEMAPHORE 10 /**< Number of semaphores per image */
+#define SEMAPHORE_MAXVAL    10            /**< maximum value for each of the semaphore, mitigates warm-up time when processes catch up with data that has accumulated */
+#define SEMAPHORE_INITVAL    0            /**< maximum value for each of the semaphore, mitigates warm-up time when processes catch up with data that has accumulated */
+#define IMAGE_NB_SEMAPHORE  10            /**< Number of semaphores per image */
 
 // Data types are defined as machine-independent types for portability
 
-#define _DATATYPE_UINT8 1 /**< uint8_t       = char */
-#define SIZEOF_DATATYPE_UINT8 1
+#define _DATATYPE_UINT8                                1  /**< uint8_t       = char */
+#define SIZEOF_DATATYPE_UINT8                          1
 
-#define _DATATYPE_INT8 2 /**< int8_t   */
-#define SIZEOF_DATATYPE_INT8 1
+#define _DATATYPE_INT8                                 2  /**< int8_t   */
+#define SIZEOF_DATATYPE_INT8                           1
 
-#define _DATATYPE_UINT16 3 /**< uint16_t      usually = unsigned short int */
-#define SIZEOF_DATATYPE_UINT16 2
+#define _DATATYPE_UINT16                               3  /**< uint16_t      usually = unsigned short int */
+#define SIZEOF_DATATYPE_UINT16                         2
 
-#define _DATATYPE_INT16 4 /**< int16_t       usually = short int          */
-#define SIZEOF_DATATYPE_INT16 2
+#define _DATATYPE_INT16                                4  /**< int16_t       usually = short int          */
+#define SIZEOF_DATATYPE_INT16                          2
 
-#define _DATATYPE_UINT32 5 /**< uint32_t      usually = unsigned int       */
-#define SIZEOF_DATATYPE_UINT32 4
+#define _DATATYPE_UINT32                               5  /**< uint32_t      usually = unsigned int       */
+#define SIZEOF_DATATYPE_UINT32                         4
 
-#define _DATATYPE_INT32 6 /**< int32_t       usually = int                */
-#define SIZEOF_DATATYPE_INT32 4
+#define _DATATYPE_INT32                                6  /**< int32_t       usually = int                */
+#define SIZEOF_DATATYPE_INT32                          4
 
-#define _DATATYPE_UINT64 7 /**< uint64_t      usually = unsigned long      */
-#define SIZEOF_DATATYPE_UINT64 8
+#define _DATATYPE_UINT64                               7  /**< uint64_t      usually = unsigned long      */
+#define SIZEOF_DATATYPE_UINT64                         8
 
-#define _DATATYPE_INT64 8 /**< int64_t       usually = long               */
-#define SIZEOF_DATATYPE_INT64 8
+#define _DATATYPE_INT64                                8  /**< int64_t       usually = long               */
+#define SIZEOF_DATATYPE_INT64                          8
 
-#define _DATATYPE_HALF \
-  13 /**< IEE 754 half-precision 16-bit (uses uint16_t for storage) */
-#define SIZEOF_DATATYPE_HALF 2
+#define _DATATYPE_HALF                                13  /**< IEE 754 half-precision 16-bit (uses uint16_t for storage) */
+#define SIZEOF_DATATYPE_HALF                           2
 
-#define _DATATYPE_FLOAT \
-  9 /**< IEEE 754 single-precision binary floating-point format: binary32 */
-#define SIZEOF_DATATYPE_FLOAT 4
+#define _DATATYPE_FLOAT                                9  /**< IEEE 754 single-precision binary floating-point format: binary32 */
+#define SIZEOF_DATATYPE_FLOAT                          4
 
-#define _DATATYPE_DOUBLE \
-  10 /**< IEEE 754 double-precision binary floating-point format: binary64 */
-#define SIZEOF_DATATYPE_DOUBLE 8
+#define _DATATYPE_DOUBLE                              10 /**< IEEE 754 double-precision binary floating-point format: binary64 */
+#define SIZEOF_DATATYPE_DOUBLE                         8
 
-#define _DATATYPE_COMPLEX_FLOAT 11 /**< complex_float  */
-#define SIZEOF_DATATYPE_COMPLEX_FLOAT 8
+#define _DATATYPE_COMPLEX_FLOAT                       11  /**< complex_float  */
+#define SIZEOF_DATATYPE_COMPLEX_FLOAT                  8
 
-#define _DATATYPE_COMPLEX_DOUBLE 12 /**< complex double */
-#define SIZEOF_DATATYPE_COMPLEX_DOUBLE 16
+#define _DATATYPE_COMPLEX_DOUBLE                      12  /**< complex double */
+#define SIZEOF_DATATYPE_COMPLEX_DOUBLE                16
 
-#define _DATATYPE_EVENT_UI8_UI8_UI16_UI8 20
-#define SIZEOF_DATATYPE_EVENT_UI8_UI8_UI16_UI8 5
+#define _DATATYPE_EVENT_UI8_UI8_UI16_UI8              20
+#define SIZEOF_DATATYPE_EVENT_UI8_UI8_UI16_UI8         5
 
-#define Dtype 9   /**< default data type for floating point */
-#define CDtype 11 /**< default data type for complex */
+#define Dtype                                          9   /**< default data type for floating point */
+#define CDtype                                        11   /**< default data type for complex */
 
 // Type of stream
 
-#define CIRCULAR_BUFFER \
-  0x0001  // Circular buffer, slice z axis is encoding time -> record writetime
-// array
-#define MATH_DATA 0x0002  // Image is mathematical vector or matrix
-#define IMG_RECV 0x0004   // Image is stream received from another computer
-#define IMG_SENT 0x0008   // Image is stream sent to other computer
+#define CIRCULAR_BUFFER  0x0001  /**< Circular buffer, slice z axis is encoding time -> record writetime array */
+#define MATH_DATA        0x0002  /**< Image is mathematical vector or matrix */
+#define IMG_RECV         0x0004  /**< Image is stream received from another computer */
+#define IMG_SENT         0x0008  /**< Image is stream sent to other computer */
 
 // axis0 definition
 
-#define ZAXIS_UNDEF 0x00000       // undefined (default)
-#define ZAXIS_SPACIAL 0x10000     // spatial coordinate
-#define ZAXIS_TEMPORAL 0x20000    // temporal coordinate
-#define ZAXIS_WAVELENGTH 0x30000  // wavelength coordinate
-#define ZAXIS_MAPPING 0x40000     // mapping index
+#define ZAXIS_UNDEF      0x00000  /**< undefined (default) */
+#define ZAXIS_SPACIAL    0x10000  /**< spatial coordinate */
+#define ZAXIS_TEMPORAL   0x20000  /**< temporal coordinate */
+#define ZAXIS_WAVELENGTH 0x30000  /**< wavelength coordinate */
+#define ZAXIS_MAPPING    0x40000  /**< mapping index */
 
 /** @brief  Keyword
  * The IMAGE_KEYWORD structure includes :
@@ -151,22 +141,21 @@ extern "C" {
  * 	- value
  */
 typedef struct {
-    char name[16]; /**< keyword name */
-    char type;     /**< N: unused, L: long, D: double, S: 16-char string     */
+    char name[16];         /**< keyword name                                                   */
+    char type;             /**< N: unused, L: long, D: double, S: 16-char string               */
     uint64_t : 0;  // align array to 8-byte boundary for speed
 
     union {
         int64_t numl;
-        double numf;
-        char valstr[16];
+        double  numf;
+        char    valstr[16];
     } value;
 
     char comment[80];
 #ifdef DATA_PACKED
 } __attribute__((__packed__)) IMAGE_KEYWORD;
 #else
-}
-IMAGE_KEYWORD;
+} IMAGE_KEYWORD;
 #endif
 
 /** @brief structure holding two 8-byte integers
@@ -280,8 +269,8 @@ typedef struct
 
     // relative timers using time relative to process start
 
-    //   double creationtime;             /**< Creation / load time of data structure (since process start)  */
-    //    double lastaccesstime;           /**< last time the image was accessed  (since process start)                      */
+    // double creationtime;             /**< Creation / load time of data structure (since process start)  */    
+    // double lastaccesstime;           /**< last time the image was accessed  (since process start)                      */
 
 
 
@@ -299,15 +288,14 @@ typedef struct
 
 
 
-    uint8_t  shared;                  /**< 1 if in shared memory                                                        */
-    int8_t   location;                /**< -1 if in CPU memory, >=0 if in GPU memory on `location` device               */
-    uint8_t  status;              	  /**< 1 to log image (default); 0 : do not log: 2 : stop log (then goes back to 2) */
-    uint64_t flag;                    /**< bitmask, encodes read/write permissions.... NOTE: enum instead of defines */
-    uint64_t *flagarray;              /**<  flag for each slice if needed (depends on imagetype) */
+    uint8_t  shared;                   /**< 1 if in shared memory                                                        */
+    int8_t   location;                 /**< -1 if in CPU memory, >=0 if in GPU memory on `location` device               */
+    uint8_t  status;                   /**< 1 to log image (default); 0 : do not log: 2 : stop log (then goes back to 2) */
+    uint64_t flag;                     /**< bitmask, encodes read/write permissions.... NOTE: enum instead of defines */
+    uint64_t *flagarray;               /**<  flag for each slice if needed (depends on imagetype) */
 
-    uint8_t  logflag;                    /**< set to 1 to start logging         */
-    uint16_t sem;
-    /**< number of semaphores in use, specified at image creation      */
+    uint8_t  logflag;                  /**< set to 1 to start logging         */
+    uint16_t sem;                      /**< number of semaphores in use, specified at image creation      */
 
 
     uint64_t : 0; // align array to 8-byte boundary for speed
@@ -343,17 +331,15 @@ typedef struct
  */
 typedef struct /**< structure used to store data arrays                      */
 {
-    char
-    name[80]; /**< local name (can be different from name in shared memory) */
+    char name[80];     /**< local name (can be different from name in shared memory) */
     // mem offset = 80
 
     /** @brief Image usage flag
      *
      * 1 if image is used, 0 otherwise. \n
-     * This flag is used when an array of IMAGE type is held in memory as a way to
-     * store multiple images. \n When an image is freed, the corresponding memory
-     * (in array) is freed and this flag set to zero. \n The active images can be
-     * listed by looking for IMAGE[i].used==1 entries.\n
+     * This flag is used when an array of IMAGE type is held in memory as a way to store multiple images. \n
+     * When an image is freed, the corresponding memory (in array) is freed and this flag set to zero. \n
+     * The active images can be listed by looking for IMAGE[i].used==1 entries.\n
      *
      */
     uint8_t used;
@@ -362,29 +348,23 @@ typedef struct /**< structure used to store data arrays                      */
 
     uint64_t memsize; /**< total size in memory if shared    */
 
-    sem_t *semlog; /**< pointer to semaphore for logging  (8 bytes on 64-bit
-                    system) */
+    sem_t *semlog; /**< pointer to semaphore for logging  (8 bytes on 64-bit system) */
 
     IMAGE_METADATA *md;
 
-    uint64_t : 0;  // align array to 8-byte boundary for speed
+    uint64_t : 0; // align array to 8-byte boundary for speed
 
     /** @brief data storage array
      *
-     * The array is declared as a union, so that multiple data types can be
-     * supported
+     * The array is declared as a union, so that multiple data types can be supported \n
      *
-     * For 2D image with pixel indices ii (x-axis) and jj (y-axis), the pixel
-     * values are stored as array.<TYPE>[ jj * md[0].size[0] + ii ] image
-     * md[0].size[0] is x-axis size, md[0].size[1] is y-axis size
+     * For 2D image with pixel indices ii (x-axis) and jj (y-axis), the pixel values are stored as array.<TYPE>[ jj * md[0].size[0] + ii ] \n
+     * image md[0].size[0] is x-axis size, md[0].size[1] is y-axis size
      *
-     * For 3D image with pixel indices ii (x-axis), jj (y-axis) and kk (z-axis),
-     * the pixel values are stored as array.<TYPE>[ kk * md[0].size[1] *
-     * md[0].size[0] + jj * md[0].size[0] + ii ] image md[0].size[0] is x-axis
-     * size, md[0].size[1] is y-axis size, md[0].size[2] is z-axis size
+     * For 3D image with pixel indices ii (x-axis), jj (y-axis) and kk (z-axis), the pixel values are stored as array.<TYPE>[ kk * md[0].size[1] * md[0].size[0] + jj * md[0].size[0] + ii ] \n
+     * image md[0].size[0] is x-axis size, md[0].size[1] is y-axis size, md[0].size[2] is z-axis size
      *
-     * @note Up to this point, all members of the structure have a fixed memory
-     * offset to the start point
+     * @note Up to this point, all members of the structure have a fixed memory offset to the start point
      */
     union {
         void *raw;  // raw pointer
@@ -410,15 +390,13 @@ typedef struct /**< structure used to store data arrays                      */
     } array; /**< pointer to data array */
 
 
-    sem_t **semptr; /**< array of pointers to semaphores   (each 8 bytes on 64-bit
-                     system) */
+    sem_t **semptr;                    /**< array of pointers to semaphores   (each 8 bytes on 64-bit system) */
 
     IMAGE_KEYWORD *kw;
 
     // PID of process that read shared memory stream
-    // Initialized at 0. Otherwise, when process is waiting on semaphore, its PID
-    // is written in this array The array can be used to look for available
-    // semaphores
+    // Initialized at 0. Otherwise, when process is waiting on semaphore, its PID is written in this array
+    // The array can be used to look for available semaphores
     pid_t *semReadPID;
 
     // PID of the process writing the data

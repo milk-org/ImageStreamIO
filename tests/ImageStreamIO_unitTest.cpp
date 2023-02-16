d82c7b70d8998f05e59056b5c285ab8e297d083c
Brian Carcich <briantcarcich@gmail.com>
2023-02-16 13:16:15 -0500

    Clean up unit test logic

--- tests/ImageStreamIO_unitTest.cpp
+++ tests/ImageStreamIO_unitTest.cpp
@@ -3,122 +3,147 @@
 #include <limits.h>
 
 /* A prefix to all names indicating ImageStreamIO Unit Tests */
-#define SHM_NAME_PREFIX "__ISIOUTs__"
+#define SHM_NAME_PREFIX    "__ISIOUTs__"
+#define SHM_NAME_ImageTest SHM_NAME_PREFIX "ImageTest"
+#define SHM_NAME_CubeTest  SHM_NAME_PREFIX "CubeTest"
+#define SHM_NAME_LocnTest  SHM_NAME_PREFIX "LocationTest"
 
 namespace {
 
-TEST(ImageStreamIOTestCreation, ImageCPUCreation) {
+  uint32_t dims2[2] = {32, 32};
+  uint32_t dims3[3] = {16, 16, 13};
   IMAGE imageTest;
-  uint32_t dims[2] = {512, 512};
+  IMAGE circularbufferTest;
 
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_PREFIX "ImageTest", 2,
-                                                     dims, _DATATYPE_FLOAT, -1,
-                                                     0, 10, 10, MATH_DATA,0));
+  int8_t cpuLocn = -1;   // Location of -1 => CPU-based shmim
+  int8_t gpuLocn =  0;   // Location of  0 => Pretend GPU-based shmim
+  int8_t badLocn = -2;   // Location of -2 => bad location
+
+////////////////////////////////////////////////////////////////////////
+// ImageStreamIO_creatIM_gpu - create  a shmim file 
+////////////////////////////////////////////////////////////////////////
+TEST(ImageStreamIOTestCreation, ImageCPUCreation) {
+
+  char SM_fname[200];
+  ImageStreamIO_filename(SM_fname, 200, SHM_NAME_ImageTest);
+
+  fprintf(stderr,"[%s=>%s]=SM_fname\n",SHM_NAME_ImageTest, SM_fname);
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_ImageTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,cpuLocn, 0, 10, 10, MATH_DATA,0)
+           );
 }
 
 TEST(ImageStreamIOTestCreation, ImageCPUSharedCreation) {
-  IMAGE imageTest;
-  uint32_t dims[2] = {512, 512};
 
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_PREFIX "ImageTest", 2,
-                                                     dims, _DATATYPE_FLOAT, -1,
-                                                     1, 10, 10, MATH_DATA,0));
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_ImageTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,cpuLocn, 1, 10, 10, MATH_DATA,0)
+           );
+}
+
+TEST(ImageStreamIOTestCreation, CubeCPUSharedCreationFailureDimension) {
+
+  EXPECT_EQ(IMAGESTREAMIO_INVALIDARG
+           ,ImageStreamIO_createIm_gpu(&circularbufferTest, SHM_NAME_CubeTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,cpuLocn, 1, 10, 10, CIRCULAR_BUFFER,1)
+           );
+}
+
+TEST(ImageStreamIOTestCreation, CubeCPUSharedCreation) {
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_createIm_gpu(&circularbufferTest, SHM_NAME_CubeTest
+                                      ,3, dims3,_DATATYPE_FLOAT
+                                      ,cpuLocn, 1, 10, 10, CIRCULAR_BUFFER,1)
+           );
+}
+
+////////////////////////////////////////////////////////////////////////
+// ImageStreamIO_OpenIm - opening an existing shmim file 
+// - Use ImageTest from above; assume it has not been destroyed
+////////////////////////////////////////////////////////////////////////
+TEST(ImageStreamIOTestOpen, ImageCPUSharedOpen) {
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_openIm(&imageTest, SHM_NAME_ImageTest)
+           );
+}
+
+TEST(ImageStreamIOTestOpen, ImageCPUSharedOpenNotExist) {
+
+  EXPECT_EQ(IMAGESTREAMIO_FILEOPEN
+           ,ImageStreamIO_openIm(&imageTest
+                                ,SHM_NAME_ImageTest "DoesNotExist")
+           );
+}
+
+TEST(ImageStreamIOTestOpen, CubeCPUSharedOpen) {
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_openIm(&circularbufferTest, SHM_NAME_CubeTest)
+           );
+}
+
+TEST(ImageStreamIOTestRead, ImageCPUSharedNbSlices) {
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_read_sharedmem_image_toIMAGE(SHM_NAME_ImageTest
+                                                      ,&imageTest)
+           );
+  EXPECT_EQ(1, ImageStreamIO_nbSlices(&imageTest));
+}
+
+TEST(ImageStreamIOTestRead, CubeCPUSharedNbSlices) {
+
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_read_sharedmem_image_toIMAGE(SHM_NAME_CubeTest
+                                                      ,&circularbufferTest)
+           );
+  EXPECT_EQ(13, ImageStreamIO_nbSlices(&circularbufferTest));
+}
+
+////////////////////////////////////////////////////////////////////////
+// Location-related tests
+////////////////////////////////////////////////////////////////////////
+TEST(ImageStreamIOTestLocation, BadLocation) {
+
+  EXPECT_EQ(IMAGESTREAMIO_FAILURE
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_LocnTest
+                                      ,2, dims2,_DATATYPE_FLOAT
+                                      ,badLocn, 1, 10, 10, MATH_DATA,0)
+           );
 }
 
 #ifdef HAVE_CUDA
 TEST(ImageStreamIOTestCreation, ImageGPUSharedCreation) {
-  IMAGE imageTest;
-  uint32_t dims[2] = {512, 512};
 
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_PREFIX "ImageTest", 2,
-                                                     dims, _DATATYPE_FLOAT, 0,
-                                                     1, 10, 10, MATH_DATA,0));
+  EXPECT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_LocnTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,gpuLocn, 1, 10, 10, MATH_DATA,0)
+           );
 }
 #endif
 
-TEST(ImageStreamIOTestCreation, CubeCPUSharedCreationFailureDimension) {
-  IMAGE circularbufferTest;
-  uint32_t dims[2] = {512, 512};
+// For GPU-based shmim, existing file (ImageTest from above) is an error
+TEST(ImageStreamIOTestLocation, InitCpuLocation) {
 
-  EXPECT_EQ(IMAGESTREAMIO_INVALIDARG, ImageStreamIO_createIm_gpu(
-                              &circularbufferTest, SHM_NAME_PREFIX "CubeTest", 2, dims,
-                              _DATATYPE_FLOAT, -1, 1, 10, 10, CIRCULAR_BUFFER,1));
-}
+  ASSERT_EQ(IMAGESTREAMIO_SUCCESS
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_LocnTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,cpuLocn, 1, 10, 10, MATH_DATA,0)
+           );
 
-TEST(ImageStreamIOTestCreation, CubeCPUSharedCreation) {
-  IMAGE circularbufferTest;
-  uint32_t dims[3] = {512, 512, 13};
-
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_createIm_gpu(
-                              &circularbufferTest, SHM_NAME_PREFIX "CubeTest", 3, dims,
-                              _DATATYPE_FLOAT, -1, 1, 10, 10, CIRCULAR_BUFFER,1));
-  EXPECT_EQ(circularbufferTest.array.raw, (void*)circularbufferTest.array.UI32);
-}
-
-TEST(ImageStreamIOTestOpen, ImageCPUSharedOpen) {
-  IMAGE imageTest;
-
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_openIm(&imageTest, SHM_NAME_PREFIX "ImageTest"));
-  EXPECT_EQ(imageTest.array.raw, (void*)imageTest.array.UI16);
-}
-
-TEST(ImageStreamIOTestOpen, ImageCPUSharedOpenNotExist) {
-  IMAGE imageTest;
-
-  EXPECT_EQ(IMAGESTREAMIO_FILEOPEN, ImageStreamIO_openIm(&imageTest, SHM_NAME_PREFIX "ImageTestNo"));
-  EXPECT_EQ(imageTest.array.raw, (void*)imageTest.array.SI16);
-}
-
-TEST(ImageStreamIOTestOpen, CubeCPUSharedOpen) {
-  IMAGE circularbufferTest;
-
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS,
-            ImageStreamIO_openIm(&circularbufferTest, SHM_NAME_PREFIX "CubeTest"));
-  EXPECT_EQ(circularbufferTest.array.raw, (void*)circularbufferTest.array.UI16);
-}
-
-TEST(ImageStreamIOTestRead, ImageCPUSharedNbSlices) {
-  IMAGE imageTest;
-  ImageStreamIO_read_sharedmem_image_toIMAGE(SHM_NAME_PREFIX "ImageTest", &imageTest);
-  EXPECT_EQ(1, ImageStreamIO_nbSlices(&imageTest));
-  EXPECT_EQ(imageTest.array.raw, (void*)imageTest.array.UI8);
-}
-
-TEST(ImageStreamIOTestRead, CubeCPUSharedNbSlices) {
-  IMAGE circularbufferTest;
-  ImageStreamIO_read_sharedmem_image_toIMAGE(SHM_NAME_PREFIX "CubeTest", &circularbufferTest);
-  EXPECT_EQ(13, ImageStreamIO_nbSlices(&circularbufferTest));
-  EXPECT_EQ(circularbufferTest.array.raw, (void*)circularbufferTest.array.SI8);
-}
-
-TEST(ImageStreamIOTestLocationBad, BadLocation) {
-  IMAGE imageTest;
-  uint32_t dims[2] = {64, 64};
-  int8_t location = -2;
-
-  EXPECT_EQ(IMAGESTREAMIO_FAILURE, ImageStreamIO_createIm_gpu(
-                              &imageTest, SHM_NAME_PREFIX "LocationTest", 2, dims,
-                              _DATATYPE_FLOAT, location, 1, 10, 10, MATH_DATA,0));
-}
-
-TEST(ImageStreamIOTestLocationInit, InitLocation) {
-  IMAGE imageTest;
-  uint32_t dims[2] = {64, 64};
-  int8_t location = -1;
-
-  EXPECT_EQ(IMAGESTREAMIO_SUCCESS, ImageStreamIO_createIm_gpu(
-                              &imageTest, SHM_NAME_PREFIX "LocationTest", 2, dims,
-                              _DATATYPE_FLOAT, location, 1, 10, 10, MATH_DATA,0));
-}
-
-TEST(ImageStreamIOTestLocationExists, ExistsLocation) {
-  IMAGE imageTest;
-  uint32_t dims[2] = {64, 64};
-  int8_t location = 0;       // Location of 0 => Pretend GPU-based shmim
-
-  EXPECT_EQ(IMAGESTREAMIO_FILEEXISTS, ImageStreamIO_createIm_gpu(
-                              &imageTest, SHM_NAME_PREFIX "LocationTest", 2, dims,
-                              _DATATYPE_FLOAT, location, 1, 10, 10, MATH_DATA,0));
+  EXPECT_EQ(IMAGESTREAMIO_FILEEXISTS
+           ,ImageStreamIO_createIm_gpu(&imageTest, SHM_NAME_LocnTest
+                                      ,2, dims2, _DATATYPE_FLOAT
+                                      ,gpuLocn, 1, 10, 10, MATH_DATA,0)
+           );
 }
 
 } // namespace

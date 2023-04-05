7501977aa78898ba0c54c4520b4da7cedc0f1c2a
Brian Carcich <briantcarcich@gmail.com>
2023-04-05 19:20:54 -0400

    Update ImCreate_test.c to ImageStreamIO library
    - Remove #include of ImageStruct.h (already included with ImageStreamIO.h)
    - Initialize parameters to ImageStreamIO_createIM within declarations
    - Make IMAGE and dimensions arrays local not malloced (nor freed)
    - Remove unused variables (x, y, s, semval)
    - Change angle step (dangle) to an exact value
    - Add missing parameter to ImageStreamIO_createIM call
    - Remove tabs; change to 4-space indents
    - Use imarray->md-> instead of imarray[0].md[0].
    - Use M_PI from math.h instead of 3.14...
    - Reverse order of nested loops for speed
    - Remove outer loop-dependent calculations from inner loop
    - Pre-calculate dy*dy
    - Allow compiler to promote integers implicitly to floats within expressions
    - Use incrementing pointer to address image elements
    - Correct cosine scaling from 0.03 to 0.003; eliminates pixel values < 0
    - Use imarray instead of &imarray[0]
    - Correct inaccurate comments; improve comments generally

--- ImCreate_test.c
+++ ImCreate_test.c
@@ -1,143 +1,91 @@
 /*
  * Example code to write image in shared memory
- * 
+ *
  * compile with:
- * gcc ImCreate_test.c ImageStreamIO.c -lm -lpthread 
- * 
+ * gcc ImCreate_test.c ImageStreamIO.c -lm -lpthread
+ *
  * Required files in compilation directory :
  * ImCreate_test.c   : source code (this file)
  * ImageStreamIO.c   : ImageStreamIO source code
  * ImageStreamIO.h   : ImageCreate function prototypes
  * ImageStruct.h     : Image structure definition
- * 
+ *
  * EXECUTION:
- * ./a.out  
+ * ./a.out
  * (no argument)
- * 
+ *
  * Creates an image imtest00 in shared memory
- * Updates the image every ~ 10ms, forever...
- * A square is rotating around the center of the image
- * 
+ * Updates the image every ~ 1ms, forever...
+ * A disk is rotating around the center of the image
+ *
  */
 
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include "ImageStruct.h"
+#include <stdio.h>
+//#include <stdlib.h>
 #include "ImageStreamIO.h"
-
-
-
 
 int main()
 {
-	IMAGE *imarray;    // pointer to array of images
-	int NBIMAGES = 1;  // can hold 1 image
-	long naxis;        // number of axis
-	uint8_t atype;     // data type
-	uint32_t *imsize;  // image size 
-	int shared;        // 1 if image in shared memory
-	int NBkw;          // number of keywords supported
+    IMAGE imarray[1];              // pointer to array of images
+    uint32_t imsize[2] = { 512, 512 }; // image size is 512 x 512
+    long naxis = sizeof(imsize) / (sizeof *imsize);  // # of axes
 
+    // Data type; see file ImageStruct.h for list of supported types
+    uint8_t atype = _DATATYPE_FLOAT;
 
+    int shared = 1;                // 1 if image in shared mem
+    int NBkw = 10;                 // number of keywords allowed
 
-	
+    // create an image in shared memory
+    ImageStreamIO_createIm(imarray, "imtest00", naxis, imsize, atype, shared, NBkw, 1);
 
-	// allocate memory for array of images
-	imarray = (IMAGE*) malloc(sizeof(IMAGE)*NBIMAGES);
+    long ii, jj;              // Image column and row indices
+    float x0, y0, xc, yc;     // Image center; disk center
+    long dtus = 1000;         // Wait 1ms = 1000 microseconds
+    float dangle = 1/64.0;    // Angle step size
+    float angle = 0.0;        // Angle of disc center, CW from right
+    float r = 100.0;          // Radius of disk center from image center
 
-	
-	// image will be 2D
-	naxis = 2;
-	
-	// image size will be 512 x 512
-	imsize = (uint32_t *) malloc(sizeof(uint32_t)*naxis);
-	imsize[0] = 512;
-	imsize[1] = 512;
-	
-	// image will be float type
-	// see file ImageStruct.h for list of supported types
-	atype = _DATATYPE_FLOAT;
-	
-	// image will be in shared memory
-	shared = 1;
-	
-	// allocate space for 10 keywords
-	NBkw = 10;
+    x0 = 0.5*imarray->md->size[0];           // Calculate enter of image
+    y0 = 0.5*imarray->md->size[1];
 
+    // Writes a disk in image
+    // - Location of disk center rotates around image center
+    while (1)
+    {
+        // Calculate disk center location based on angle,
+        // then increment angle for the next loop pass
+        xc = x0 + r*cos(angle);
+        yc = y0 + r*sin(angle);
+        angle += dangle;
+        if(angle > 2.0*M_PI) { angle -= 2.0 * M_PI; }
 
-	
-	// create an image in shared memory
-	ImageStreamIO_createIm(&imarray[0], "imtest00", naxis, imsize, atype, shared, NBkw);
+        imarray->md->write = 1;         // Poor-man's mutex when writing
 
+        // ->array is union; ->array.F is float pointer to image
+        float* dotF = imarray->array.F;
+        for(jj=0; jj<imarray->md->size[1]; jj++)            // loop rows
+        {
+            float dy = jj-yc;
+            float dy2 = dy*dy;
+            for(ii=0; ii<imarray->md->size[0]; ii++)     // loop columns
+            {
+                float dx = ii-xc;
+                // Brightness is highest at the center
+                // of the disk and fades radially
+                *(dotF++) = cos(0.003*dx)*cos(0.003*dy)*exp(-1.0e-4*(dx*dx+dy2));
 
+            }
+        }
+        // Post all semaphores (index = -1)
+        ImageStreamIO_sempost(imarray, -1);
 
-	float angle; 
-	float r;
-	float r1;
-	long ii, jj;
-	float x, y, x0, y0, xc, yc;
-	// float squarerad=20;
-	long dtus = 1000; // update every 1ms
-	float dangle = 0.02;
-	
-	int s;
-	int semval;
+        imarray->md->write = 0; // Done writing; release mutex
+        imarray->md->cnt0++;
+        imarray->md->cnt1++;
 
-	// writes a square in image
-	// square location rotates around center
-	angle = 0.0;
-	r = 100.0;
-	x0 = 0.5*imarray[0].md[0].size[0];
-	y0 = 0.5*imarray[0].md[0].size[1];
-	while (1)
-	{
-		// disk location
-		xc = x0 + r*cos(angle);
-		yc = y0 + r*sin(angle);
-		
-		
-		imarray[0].md[0].write = 1; // set this flag to 1 when writing data
-		
-		for(ii=0; ii<imarray[0].md[0].size[0]; ii++)
-			for(jj=0; jj<imarray[0].md[0].size[1]; jj++)
-			{
-				x = 1.0*ii;
-				y = 1.0*jj;
-				float dx = x-xc;
-				float dy = y-yc;
-				
-				imarray[0].array.F[jj*imarray[0].md[0].size[0]+ii] = cos(0.03*dx)*cos(0.03*dy)*exp(-1.0e-4*(dx*dx+dy*dy));
-				
-				//if( (x-xc<squarerad) && (x-xc>-squarerad) && (y-yc<squarerad) && (y-yc>-squarerad))
-				//	imarray[0].array.F[jj*imarray[0].md[0].size[0]+ii] = 1.0;
-				//else
-				//	imarray[0].array.F[jj*imarray[0].md[0].size[0]+ii] = 0.0;
-			}
-		
-		// POST ALL SEMAPHORES
-		ImageStreamIO_sempost(&imarray[0], -1);
-		
-		imarray[0].md[0].write = 0; // Done writing data
-		imarray[0].md[0].cnt0++;
-		imarray[0].md[0].cnt1++;
-		
-		
-		usleep(dtus);
-		angle += dangle;
-		if(angle > 2.0*3.141592)
-			angle -= 2.0*3.141592;
-		//printf("Wrote square at position xc = %16f  yc = %16f\n", xc, yc);
-		//fflush(stdout);
-	}
-	
-
-
-	free(imsize);
-	free(imarray);
-	
-	return 0;
+        usleep(dtus);           // Wait 1ms
+    }
+    return 0;
 }

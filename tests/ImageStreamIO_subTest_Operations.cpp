7161cf7431e32a06ce534a94b22a67133e620d93
Brian Carcich <briantcarcich@gmail.com>
2023-04-07 17:30:37 -0400

    Improve comments

--- tests/ImageStreamIO_subTest_Operations.cpp
+++ tests/ImageStreamIO_subTest_Operations.cpp
@@ -9,10 +9,15 @@
 #define SHM_NAME_PREFIX    "__ISIOUTs__"
 #define SHM_NAME_OpsTest SHM_NAME_PREFIX "OpsTest"
 
+
+////////////////////////////////////////////////////////////////////////
+// Forked child data, accessible to SIGCHLD handler
 static int child_active = false;
 static int child_wstatus = 0;
 static pid_t child_pid = 0;
 
+
+////////////////////////////////////////////////////////////////////////
 // Handle signal when forked child exits
 static void
 sigchld_handler(int sig)
@@ -26,6 +31,10 @@ sigchld_handler(int sig)
     }
     errno = saved_errno;
 }
+
+
+////////////////////////////////////////////////////////////////////////
+// Install sigchld_handler(...) action (above) for SIGCHLD signal
 static int
 install_sigchld_handler()
 {
@@ -35,7 +44,7 @@ install_sigchld_handler()
     struct sigaction sa;
     sa.sa_handler = &sigchld_handler;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;//SA_RESTART | SA_NOCLDSTOP | SA_NOCLDWAIT;
+    sa.sa_flags = 0;
     if (sigaction(SIGCHLD, &sa, 0) == -1)
     {
         perror("Error in performing SIGCHLD handler install");
@@ -45,18 +54,26 @@ install_sigchld_handler()
     return 0;
 }
 
+
+////////////////////////////////////////////////////////////////////////
+// Operational test of ImageStreamIO library
+//
+// Each test will increment test_count by 1;
+// Each test successfully completed will increment success_count
+// If test_count == success_count on return, then all tests succeeded
 void
 ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
 {
-    // Each test will increment test_count by 1;
-    // Each test successfully completed will increment success_count
     test_count = 0;
     success_count = 0;
 
+    // Install the SIGCHLD handler action
     ++test_count;
     if (install_sigchld_handler()) { return; }
     ++success_count;
 
+    // Open unnnamed pipes to initially communicate with forked child
+    // process until ImageStreamIO shmim is ready
     ++test_count;
     int pipes[2];
     if (pipe2(pipes,O_NONBLOCK) < 0) { return; }
@@ -72,10 +89,16 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
     }
     ++success_count;
 
+    // Determine which process this is from successful fork
+    // Both child and parent processes execute the rest of the code here
+    // - The parent will return to the caller, runnin tests until then
+    // - The child with exit(...)
+
     // Child receives 0 returned from fork; parent receives PID of child
     bool i_am_child = fork_pid==0;
     bool i_am_parent = !i_am_child;
 
+    // ImageStreamIO IMAGE structure with shmim confiuration and mapping 
     IMAGE parent_image = { 0 };
     IMAGE child_image = { 0 };
 
@@ -86,11 +109,18 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
     ////////////////////////////////////////////////////////////////////
 
     if (i_am_parent) {
+
         // Initialize parent:
 
+        // - Count all initialization steps as a single test
+        // - Variable [success_count] will not be incremented until and
+        //   unless all initialization steps complete successfully
+        // - If any step fails befor then, parent will return to caller
+        //   with mismatched test and success counts
+        // N.B. this pattern will be repeated
         ++test_count;
 
-        // - Setup static info for signal handler about forked child
+        // - Module static info for signal handler about forked child
         child_active = true;
         child_pid = fork_pid;
 
@@ -103,6 +133,8 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
                                       ,2, dims2, _DATATYPE_UINT32
                                       ,cpuLocn, shared, 10, 10
                                       ,MATH_DATA, 0)) { return; }
+
+        //   - Reset typical error from _createIm_gpu
         if (errno == ENOENT) { errno = 0; }
         ImageStreamIO_semflush(&parent_image, -1);
 
@@ -112,6 +144,8 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
         // - Close pipes; semaphores can communicate with forked child
         if (close(pipes[1])) { return; }
         if (close(pipes[0])) { return; }
+
+        // - All initialization was successful
         ++success_count;
     }
 
@@ -119,7 +153,10 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
     {
         // Initialize forked child:
 
-        // - Wait for synchronization byte on pipe
+        // - General pattern is similar to that of parent above,
+        //   but child process will exit instead of returning
+
+        // - Wait for single synchronization byte from parent (above)
         char buf[1];
         int iloop=0;
         int iread=0;
@@ -129,24 +166,31 @@ ImageStreamIO_subTest_Operations(int& test_count, int& success_count)
             iread = read(pipes[0],buf,1);
             usleep(1000);
         } while (iread!=1 && iloop++ < 999);
+
+        // - Close pipes; communicate with parent via shmim semaphores
         close(pipes[0]);
         close(pipes[1]);
+
+        // - Exit if that byte was not received
         if (iread!=1) { exit(1); }
 
-        // - Open shmim
+        // - Received byte from parent means shmim is ready; open it
         if (IMAGESTREAMIO_SUCCESS!=
             ImageStreamIO_openIm(&child_image, SHM_NAME_OpsTest))
         {
             perror("Child ImageStreamIO_openIm failed");
             exit(1);
         }
+
+        // - Reset typical error from _openIm
         if (errno == ENOENT) { errno = 0; }
     }
 
     // Child gets its PID from system call; parent already has child PID
     pid_t my_pid = i_am_child ? getpid() : 0;
 
-    // Lehmer RNG
+    // Lehmer Random Number Generator:  SEED[n+1]=(SEED[n]*A) % M
+    // - Use child PID as initial seed
     const uint32_t minstd_m = 1 << 31;
     const uint32_t minstd_a = 48271;
     uint32_t parent_seed = fork_pid % minstd_m;

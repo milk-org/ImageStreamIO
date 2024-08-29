3a54aa707290379e9c4445ed592c54b3779a1669
Vincent Deo <DasVinch@users.noreply.github.com>
2024-08-29 09:47:04 -1000

    Revert "Updated version config to mirror that of milk"
    This reverts commit 31ce189fa4e8ea6c02feab09ef8089ceb5ac4693.

--- ImageStruct.h
+++ ImageStruct.h
@@ -18,8 +18,7 @@
 #ifndef _IMAGESTRUCT_H
 #define _IMAGESTRUCT_H
 
-#include "ImageStreamIO_config.h"
-// #define IMAGESTRUCT_VERSION "2.00"
+#define IMAGESTRUCT_VERSION "2.00"
 
 #define STRINGMAXLEN_IMAGE_NAME          80
 #define STRINGMAXLEN_FILE_NAME          200
